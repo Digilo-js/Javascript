c6b2ccc047a443daa8b8d765fc8ffb8040b64628
Digilo-js <contaofficial107@gmail.com>
2024-09-10 00:16:14 +0100

    Initial commit

---
+++ Exercicios/ex1/script.js
@@ -0,0 +1,7 @@
+function caregar(){
+   var msg = document.getElementById("msg")
+   var img = window.document.getElementByI("foto")
+   var data = new Date()
+   var hora = data. getHours()
+   msg.innerHTML.hora  
+}    
